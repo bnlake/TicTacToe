2af04156330b02ebc3447d7dfe27d22d4e3ba76e
Han Kim <hanniek55@gmail.com>
2019-01-27 21:45:29 -0600

    Add layout for Random Tic Tac Toe and Activity

--- app/src/main/java/com/csci4020/tic_tac_toe/RandomActivity.java
+++ app/src/main/java/com/csci4020/tic_tac_toe/RandomActivity.java
@@ -1,9 +1,24 @@
 package com.csci4020.tic_tac_toe;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
-public class RandomActivity extends Activity {
+public class RandomActivity extends Activity implements View.OnClickListener {
 
-    
+    private boolean gameOver = false;
+    private int[][] gameBoardSquares = new int[3][3];
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_random);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
 }

0469937a3efff156c3b3c634aa40452b3d5b17f0
Brian Lake <bnlake@gmail.com>
2019-01-28 21:04:04 -0600

    Create & Implement About Page

---
+++ app/src/main/java/com/csci4020/tic_tac_toe/AboutActivity.java
@@ -0,0 +1,14 @@
+package com.csci4020.tic_tac_toe;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+	}
+}
--- app/src/main/java/com/csci4020/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/csci4020/tic_tac_toe/MainActivity.java
@@ -1,7 +1,16 @@
 package com.csci4020.tic_tac_toe;
 
+/*
+CSCI 4020
+Assignment 1
+Han Kim
+Brian Lake
+ */
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +18,24 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btnMenuAboutApp).setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                gotoAboutApp();
+            }
+        });
+    }
+
+    /**
+     * Create intent to open about app activity
+     */
+    private void gotoAboutApp()
+    {
+        // NOTE: JUST SETTING CONTENT VIEW ELIMINATES USE OF BACK BUTTON
+        Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+        startActivity(intent);
     }
 }

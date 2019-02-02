cedbe368e9cc3ea008f8704370e8c07f5e42b266
Han Kim <hanniek55@gmail.com>
2019-02-02 11:09:01 -0600

    New layout file and add UI for random game

--- app/src/main/java/com/csci4020/tic_tac_toe/RandomActivity.java
+++ app/src/main/java/com/csci4020/tic_tac_toe/RandomActivity.java
@@ -11,18 +11,35 @@ import android.view.View;
 
 public class RandomActivity extends Activity implements View.OnClickListener {
 
+
     private boolean gameOver = false;
     private int[][] gameBoardSquares = new int[3][3];
 
+    // TODO: Current piece will be generated randomly
+
+    public int[] gameIds = {
+            R.id.imageButton00, R.id.imageButton01, R.id.imageButton02, R.id.imageButton03,
+            R.id.imageButton04, R.id.imageButton05, R.id.imageButton06, R.id.imageButton07,
+            R.id.imageButton08
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_play_random);
+        setContentView(R.layout.activity_random);
     }
 
     @Override
     public void onClick(View v) {
 
     }
+
+    public void setupButtonListener() {
+        int[] ids = {
+                R.id.btn_o, R.id.btn_x, R.id.imageButton00, R.id.imageButton01, R.id.imageButton02,
+                R.id.imageButton03, R.id.imageButton04, R.id.imageButton05, R.id.imageButton06,
+                R.id.imageButton07, R.id.imageButton08
+        };
+    }
+
 }

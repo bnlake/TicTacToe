506590f38e9133141c090f195e0197a8a1bf8113
Han Kim <hanniek55@gmail.com>
2019-02-02 20:42:00 -0600

    Change method documentation

--- app/src/main/java/com/csci4020/tic_tac_toe/RandomActivity.java
+++ app/src/main/java/com/csci4020/tic_tac_toe/RandomActivity.java
@@ -58,7 +58,7 @@ public class RandomActivity extends Activity implements View.OnClickListener {
     }
 
     /**
-     * Give
+     * Change text when user presses a button and check if a player has win.
      */
     @Override
     public void onClick(View v) {
